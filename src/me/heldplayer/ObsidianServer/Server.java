734611421062a9d17aa284d0d1437d68d25fe3af
heldplayer <mitchel.pyl@gmail.com>
2012-06-19 20:37:56 +0200

    Create first classes for the server

---
+++ src/me/heldplayer/ObsidianServer/Server.java
@@ -0,0 +1,11 @@
+package me.heldplayer.ObsidianServer;
+
+public class Server implements Runnable {
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/me/heldplayer/ObsidianServer/ServerLauncher.java
@@ -0,0 +1,13 @@
+package me.heldplayer.ObsidianServer;
+
+public class ServerLauncher {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
