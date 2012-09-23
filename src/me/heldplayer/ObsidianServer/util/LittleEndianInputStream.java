e87c24232620111ad040660b12105850de526155
heldplayer <mitchel.pyl@gmail.com>
2012-09-23 22:41:23 +0200

    Little Endian Input and Output stream classes

---
+++ src/me/heldplayer/ObsidianServer/util/LittleEndianInputStream.java
@@ -0,0 +1,120 @@
+package me.heldplayer.ObsidianServer.util;
+
+import java.io.DataInput;
+import java.io.DataInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+public class LittleEndianInputStream extends InputStream implements DataInput {
+
+	private DataInputStream dataInputSteam;
+	private InputStream inputSteam;
+	private byte buffer[];
+
+	public LittleEndianInputStream(InputStream in) {
+		this.inputSteam = in;
+		this.dataInputSteam = new DataInputStream(in);
+		buffer = new byte[8];
+	}
+
+	@Override
+	public int available() throws IOException {
+		return dataInputSteam.available();
+	}
+
+	@Override
+	public final short readShort() throws IOException {
+		dataInputSteam.readFully(buffer, 0, 2);
+		return (short) ((buffer[1] & 0xff) << 8 | (buffer[0] & 0xff));
+	}
+
+	@Override
+	public final int readUnsignedShort() throws IOException {
+		dataInputSteam.readFully(buffer, 0, 2);
+		return ((buffer[1] & 0xff) << 8 | (buffer[0] & 0xff));
+	}
+
+	@Override
+	public final char readChar() throws IOException {
+		dataInputSteam.readFully(buffer, 0, 2);
+		return (char) ((buffer[1] & 0xff) << 8 | (buffer[0] & 0xff));
+	}
+
+	@Override
+	public final int readInt() throws IOException {
+		dataInputSteam.readFully(buffer, 0, 4);
+		return (buffer[3]) << 24 | (buffer[2] & 0xff) << 16 | (buffer[1] & 0xff) << 8 | (buffer[0] & 0xff);
+	}
+
+	@Override
+	public final long readLong() throws IOException {
+		dataInputSteam.readFully(buffer, 0, 8);
+		return (long) (buffer[7]) << 56 | (long) (buffer[6] & 0xff) << 48 | (long) (buffer[5] & 0xff) << 40 | (long) (buffer[4] & 0xff) << 32 | (long) (buffer[3] & 0xff) << 24 | (long) (buffer[2] & 0xff) << 16 | (long) (buffer[1] & 0xff) << 8 | (long) (buffer[0] & 0xff);
+	}
+
+	@Override
+	public final float readFloat() throws IOException {
+		return Float.intBitsToFloat(readInt());
+	}
+
+	@Override
+	public final double readDouble() throws IOException {
+		return Double.longBitsToDouble(readLong());
+	}
+
+	@Override
+	public final int read(byte b[], int off, int len) throws IOException {
+		return inputSteam.read(b, off, len);
+	}
+
+	@Override
+	public final void readFully(byte b[]) throws IOException {
+		dataInputSteam.readFully(b, 0, b.length);
+	}
+
+	@Override
+	public final void readFully(byte b[], int off, int len) throws IOException {
+		dataInputSteam.readFully(b, off, len);
+	}
+
+	@Override
+	public final int skipBytes(int n) throws IOException {
+		return dataInputSteam.skipBytes(n);
+	}
+
+	@Override
+	public final boolean readBoolean() throws IOException {
+		return dataInputSteam.readBoolean();
+	}
+
+	@Override
+	public final byte readByte() throws IOException {
+		return dataInputSteam.readByte();
+	}
+
+	@Override
+	public int read() throws IOException {
+		return inputSteam.read();
+	}
+
+	@Override
+	public final int readUnsignedByte() throws IOException {
+		return dataInputSteam.readUnsignedByte();
+	}
+
+	@Override
+	@Deprecated
+	public final String readLine() throws IOException {
+		return dataInputSteam.readLine();
+	}
+
+	@Override
+	public final String readUTF() throws IOException {
+		return dataInputSteam.readUTF();
+	}
+
+	@Override
+	public final void close() throws IOException {
+		dataInputSteam.close();
+	}
+}
---
+++ src/me/heldplayer/ObsidianServer/util/LittleEndianOutputStream.java
@@ -0,0 +1,113 @@
+package me.heldplayer.ObsidianServer.util;
+
+import java.io.DataOutput;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+
+public class LittleEndianOutputStream extends OutputStream implements DataOutput {
+
+	private DataOutputStream dataOutputSteam;
+	private OutputStream outputStream;
+
+	public LittleEndianOutputStream(OutputStream in) {
+		this.outputStream = in;
+		this.dataOutputSteam = new DataOutputStream(in);
+	}
+
+	public final void close() throws IOException {
+		dataOutputSteam.close();
+	}
+
+	@Override
+	public final void writeBoolean(boolean arg) throws IOException {
+		dataOutputSteam.writeBoolean(arg);
+	}
+
+	@Override
+	public final void writeByte(int arg) throws IOException {
+		dataOutputSteam.writeByte(arg & 0xff);
+	}
+
+	@Override
+	public final void writeBytes(String arg) throws IOException {
+		dataOutputSteam.writeBytes(arg);
+	}
+
+	public final void writeBytes(byte[] arg) throws IOException {
+		for (byte theByte : arg)
+			dataOutputSteam.writeByte(theByte);
+	}
+
+	@Override
+	public final void writeChar(int arg) throws IOException {
+		byte[] buffer = new byte[2];
+
+		for (int i = 0; i < buffer.length; i++) {
+			buffer[i] = (byte) ((arg >> (i * 8)) & 0xff);
+		}
+
+		dataOutputSteam.write(buffer);
+	}
+
+	@Override
+	public final void writeChars(String arg) throws IOException {
+		char[] chars = arg.toCharArray();
+
+		for (char theChar : chars)
+			writeChar(theChar);
+	}
+
+	@Override
+	public final void writeDouble(double arg) throws IOException {
+		writeLong(Double.doubleToLongBits(arg));
+	}
+
+	@Override
+	public final void writeFloat(float arg) throws IOException {
+		writeInt(Float.floatToIntBits(arg));
+	}
+
+	@Override
+	public final void writeInt(int arg) throws IOException {
+		byte[] buffer = new byte[4];
+
+		for (int i = 0; i < buffer.length; i++) {
+			buffer[i] = (byte) ((arg >> (i * 8)) & 0xff);
+		}
+
+		dataOutputSteam.write(buffer);
+	}
+
+	@Override
+	public final void writeLong(long arg) throws IOException {
+		byte[] buffer = new byte[8];
+
+		for (int i = 0; i < buffer.length; i++) {
+			buffer[i] = (byte) ((arg >> (i * 8)) & 0xff);
+		}
+
+		dataOutputSteam.write(buffer);
+	}
+
+	@Override
+	public final void writeShort(int arg) throws IOException {
+		byte[] buffer = new byte[2];
+
+		for (int i = 0; i < buffer.length; i++) {
+			buffer[i] = (byte) ((arg >> (i * 8)) & 0xff);
+		}
+
+		dataOutputSteam.write(buffer);
+	}
+
+	@Override
+	public final void writeUTF(String arg) throws IOException {
+		dataOutputSteam.writeUTF(arg);
+	}
+
+	@Override
+	public final void write(int b) throws IOException {
+		outputStream.write(b);
+	}
+}
